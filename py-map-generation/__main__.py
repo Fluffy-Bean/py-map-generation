49d431d2c125e15950a8c343b75213508b4970a3
Fluffy <michal-gdula@protonmail.com>
2023-08-15 14:33:57 +0100

    Clean up code

--- py-map-generation/__main__.py
+++ py-map-generation/__main__.py
@@ -1,16 +1,22 @@
 import random
+import numpy
 from PIL import Image
 
 
 class GenerateMap:
-    def __init__(self, dimension: tuple, c_level: int = 0, seed: int = 69, choice: list = (-100, 200)):
-        self.dimension = dimension
-        self.c_level = c_level
-        self.choice = choice
+    def __init__(self, dimension: tuple, seed: int = 621, **kwargs):
+        random.seed(seed)
+
+        self.choice = (-100, 200)
+        self.sea_level = 0
+        self.density = 50
         self.nodes = []
+
+        self.dimension = dimension
         self.seed = seed
 
-        random.seed(seed)
+        if kwargs:
+            [setattr(self, item, kwargs[item]) for item in kwargs]
 
         self._generate_nodes()
         self._get_neighbors()
@@ -31,12 +37,10 @@ class GenerateMap:
                 if n == n2:
                     continue
 
-                if n.position[0] == n2.position[0]:
-                    if n2.position[1] == n.position[1] + 1:
-                        n.neighbors.append(n2)
-                elif n.position[1] == n2.position[1]:
-                    if n2.position[0] == n.position[0] + 1:
-                        n.neighbors.append(n2)
+                if n.position[0] == n2.position[0] and n2.position[1] == n.position[1] + 1:
+                    n.neighbors.append(n2)
+                elif n.position[1] == n2.position[1] and n2.position[0] == n.position[0] + 1:
+                    n.neighbors.append(n2)
 
                 if len(n.neighbors) >= 2:
                     break
@@ -44,24 +48,22 @@ class GenerateMap:
     def _fill_nodes(self):
         for n in self.nodes:
             if 40 < n.position[0] < 160 and 20 < n.position[1] < 80:
-                if random.randint(0, 1000) <= self.seed:
+                if random.randint(0, 1000) <= self.density:
                     n.elevation = random.choice(self.choice)
                     n.temperature = random.choice(self.choice)
 
     def smooth_map(self):
         for n1 in self.nodes:
-            n1.active = False
             for n2 in n1.neighbors:
-                if n2.active == True:
-                    if n1.elevation != n2.elevation:
-                        if random.randrange(0, 100) < 5:
-                            a = (n1.elevation + n2.elevation) / 2
-                            n1.elevation = a
-                            n2.elevation = a
-                        elif random.randrange(0, 100) < 5:
-                            a = (n1.temperature + n2.temperature) / 2
-                            n1.temperature = a
-                            n2.temperature = a
+                if n1.elevation != n2.elevation:
+                    if random.randrange(0, 100) < 5:
+                        a = (n1.elevation + n2.elevation) / 2
+                        n1.elevation = a
+                        n2.elevation = a
+                    elif random.randrange(0, 100) < 5:
+                        a = (n1.temperature + n2.temperature) / 2
+                        n1.temperature = a
+                        n2.temperature = a
 
     def raise_land(self):
         for n in self.nodes:
@@ -69,7 +71,7 @@ class GenerateMap:
                 if random.randrange(0, 100) < 1:
                     n.elevation += 100
 
-    def lower_land(self):
+    def lower_sea(self):
         for n in self.nodes:
             if n.elevation <= 0:
                 if random.randrange(0, 100) < 1:
@@ -88,50 +90,29 @@ class Node:
         self.elevation = 0
         self.temperature = 0
         self.neighbors = []
-        self.active = True
-
-    def set_active(self):
-        self.active = True
 
 
 if __name__ == '__main__':
     world = GenerateMap((200, 100))
 
-    def process1():
-        [n.set_active() for n in world.nodes]
-        world.smooth_map()
-
-    def process2():
-        [n.set_active() for n in world.nodes]
-        world.lower_land()
-
-    def process3():
-        [n.set_active() for n in world.nodes]
-        world.raise_land()
-
-    def process4():
-        [n.set_active() for n in world.nodes]
-        world.raise_temperature()
-
-
     print('0')
     for i in range(100):
-        process1()
-        process4()
+        world.smooth_map()
+        world.raise_temperature()
     print('1')
     for i in range(15):
-        process2()
-        process3()
+        world.lower_sea()
+        world.raise_land()
     print('2')
-    for i in range(100):
-        process1()
+    for i in range(150):
+        world.smooth_map()
 
     img = Image.new('RGB', (200, 100), color='blue')
 
     for n in world.nodes:
         if n.elevation <= (-25):
             img.putpixel(n.position, (0, 0, 0))
-        elif n.elevation <= world.c_level:
+        elif n.elevation <= world.sea_level:
             img.putpixel(n.position, (0, 0, 255))
         elif n.elevation <= 25 and n.temperature <= 0:
             img.putpixel(n.position, (0, 255, 255))
